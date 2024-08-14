a2878370a16bf1a99b1bb90e30672b6972b5bcaa
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-14 12:19:31 +0300

    end task 5

--- Task5 Studet/bin/main.dart
+++ Task5 Studet/bin/main.dart
@@ -1,3 +1,10 @@
+import 'package:task5studet/student.dart';
+
 void main(List<String> arguments) {
-  
+  Student ahmed =
+      Student(name: 'Ahmed', age: 25, studentID: 1234, grade: 'third');
+
+  print('\n=======================================================================================');
+  print('== ${ahmed.printProperties()} ==');
+  print('=======================================================================================');
 }
