c06cf21e214898be72a263b655935b9a9408cfe2
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-14 11:54:29 +0300

    add task4

---
+++ Task4 Car/bin/task4car.dart
@@ -0,0 +1,5 @@
+import 'package:task4car/task4car.dart' as task4car;
+
+void main(List<String> arguments) {
+  print('Hello world: ${task4car.calculate()}!');
+}
---
+++ Task4 Car/test/task4car_test.dart
@@ -0,0 +1,8 @@
+import 'package:task4car/task4car.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
