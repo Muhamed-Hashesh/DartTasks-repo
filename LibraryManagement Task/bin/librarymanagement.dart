faa26ff1c47bc84275b008ccd28cc79308467418
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-15 13:07:45 +0300

    add library management task

---
+++ LibraryManagement Task/bin/librarymanagement.dart
@@ -0,0 +1,5 @@
+import 'package:librarymanagement/librarymanagement.dart' as librarymanagement;
+
+void main(List<String> arguments) {
+  print('Hello world: ${librarymanagement.calculate()}!');
+}
---
+++ LibraryManagement Task/test/librarymanagement_test.dart
@@ -0,0 +1,8 @@
+import 'package:librarymanagement/librarymanagement.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
