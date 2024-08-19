80d0de6cb0b910887d914d7372f3536691f3d630
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-19 13:27:04 +0300

    add flutter project

---
+++ sec_flutter_relearn/android/app/src/main/kotlin/com/example/sec_flutter_relearn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sec_flutter_relearn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sec_flutter_relearn/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xff333339),
+        drawer: const Drawer(),
+        appBar: AppBar(
+          backgroundColor: const Color(0xff333333),
+          title: const Text('Flutter App'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Image.network(
+                  'https://pixlr.com/images/index/product-image-one.webp'),
+            ),
+            const Text(
+              'This is an image',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            const Text(
+              'end of the column here without alignment',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ));
+  }
+}
