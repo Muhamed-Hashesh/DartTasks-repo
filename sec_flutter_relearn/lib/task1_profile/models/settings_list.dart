26e32fe168b57fb314eb0d8f52978456f54dc08a
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-19 16:14:48 +0300

    add settings_list

---
+++ sec_flutter_relearn/lib/task1_profile/models/settings_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> settingsList = [
+  {
+    'title': 'Privacy',
+    'icon': Icons.privacy_tip_outlined,
+  },
+  {
+    'title': 'Purchase History',
+    'icon': Icons.history,
+  },
+  {
+    'title': 'Help & Support',
+    'icon': Icons.help_outline,
+  },
+  {
+    'title': 'Settings',
+    'icon': Icons.settings_outlined,
+  },
+  {
+    'title': 'Invite a Friend',
+    'icon': Icons.person_add_alt_1_outlined,
+  },
+  {
+    'title': 'Logout',
+    'icon': Icons.logout_outlined,
+  },
+];
