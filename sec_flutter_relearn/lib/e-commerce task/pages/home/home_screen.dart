1410bb0f6bf59751a49e7aae3ccaf6a2203d5c85
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-08-30 23:09:57 +0300

    ecommerce home2

---
+++ sec_flutter_relearn/lib/e-commerce task/pages/home/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EcommerceHomeScreen extends StatelessWidget {
+  const EcommerceHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
